b3c21f0baf44bbda34197212cb7c0b6a348b5041
Antonio_Jr <juniormouracruz@gmail.com>
2026-06-13 11:56:38 -0300

    Adicionada as funções para manipular os arquivos

--- veterinaria/crud.py
+++ veterinaria/crud.py
@@ -9,4 +9,32 @@ def carregar(arquivo):
     arq = arquivo
     pacientes = ler_arquivo(arq)
 
-# parte pra tu antonio
+
+def adicionar_animais(pacientes, animal):
+    pacientes.append(animal)
+    print("Animalzinho adicionado!")
+    
+
+def remover_animais(pacientes, id_removido):
+    for i in range(len(pacientes)):
+        if pacientes[i]["identidade"] == id_removido:
+            pacientes.pop(i)
+            print("Animal removido!")
+            return
+    print("Animal não encontrado!")
+
+
+def buscar_animais(pacientes, id_procurado): #Dar uma olhada se os nomes das variáveis e ordem estão corretos, acredito que tenha ajustado, mas vai que...
+    for animal in pacientes:
+        if animal["identidade"] == id_procurado:
+            print(f"""
+                Id:           {animal['identidade']}
+                Nome:         {animal['nome']}
+                Espécie:      {animal['especie']}
+                Idade:        {animal['idade']}
+                Peso:         {animal['peso']}
+                Cadastro:     {animal['cadastro']}
+                Procedimento: {animal['procedimentos_formatado']}
+                
+            """)
+    print("Animal não encontrado!")
